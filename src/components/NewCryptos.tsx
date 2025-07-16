d4600fbfa9ae256c6ab91de5221ca60d972f8a60
HectorERS <hector.rios@mail.udp.cl>
2025-07-15 20:53:19 -0400

    Fixes menore de href

--- src/components/NewCryptos.tsx
+++ src/components/NewCryptos.tsx
@@ -98,6 +98,7 @@ export default function NewCryptos() {
               Ver todas las criptomonedas nuevas
             </button>
           </Link>
+          
         </div>
       </div>
     </section>
